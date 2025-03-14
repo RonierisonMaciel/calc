17f3887c34fc2cb96f184cfe31aaca4b4ab7a758
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-03-13 21:19:43 -0300

    feat: add subtracao and square functions; update calculadora and tests

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,21 @@
 # função soma	
-def soma(a:int, b:int):
+def soma(a, b):
     return a + b
 
-# função divisão
-def divisao(a:int, b:int):
+# função divisã
+def divisao(a, b):
     if b == 0:
         raise ValueError("Não é permitido a divisão por zero!")
     return a / b
 
-def multiplicacao(a:int, b:int):
+# função multiplicação
+def multiplicacao(a, b):
     return a * b
+
+# função subtração
+def subtracao(a, b):
+    return a - b
+
+# função square
+def square(a):
+    return a * a
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,14 +1,17 @@
 import pytest
-from calculadora import soma, divisao, multiplicacao
+from calculadora import soma, divisao, multiplicacao, subtracao, square
+
 
 def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, -1) == -2
     assert soma(0, 0) == 0
 
+
 def test_divisao():
     assert divisao (10, 2) == 5
     assert divisao (9, 3) == 3
+
 
 def test_division_por_zero():
     with pytest.raises(ValueError, match="Não é permitido a divisão por zero!"):
@@ -19,3 +22,17 @@ def test_multiplicacao():
     assert multiplicacao(2, 3) == 6
     assert multiplicacao(0, 0) == 0
     assert multiplicacao(1, 1) == 1
+
+
+def test_subtracao():
+    assert subtracao(2, 3) == -1
+    assert subtracao(0, 0) == 0
+    assert subtracao(1, 1) == 0
+
+
+def test_square():
+    assert square(2) == 4
+    assert square(0) == 0
+    assert square(1) == 1
+    assert square(-1) == 1
+    assert square(-2) == 4
